cfa5be1bd218452487d1aaeb9d5b9e1bc746b5af
Hammad Ali <hammad.se@live.com>
2023-01-08 16:06:10 +0500

    forms validation init

---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import { email, EmForm, EmFormError, EmFormErrorMessage, EmFormGroup, required, useEmForms } from "@enfometa/em-forms";
+
+function Login() {
+  const forms = useEmForms({
+    forms: [
+      {
+        name: "email",
+        value: "",
+        validators: [
+          { name: "required", func: required, message: "Email is required" },
+          { name: "email", func: email, message: "Invalid email" },
+        ],
+      },
+      {
+        name: "password",
+        value: "",
+        validators: [{ name: "required", func: required, message: "Password is required" }],
+      },
+    ],
+  });
+
+  const login = () => {
+    if (forms.validate()) {
+      const model = forms.toModel();
+      console.log(model);
+    }
+  };
+  return (
+    <div className="login">
+      <div>Login form</div>
+      <EmFormGroup emForms={forms}>
+        <div>
+          <EmForm formName="email">
+            <input type="email" name="email" placeholder="Email" className="form" />
+          </EmForm>
+          {/* <EmFormError formName="email" validatorName="required">
+            <div>This is a custom error message</div>
+          </EmFormError> */}
+          <div className="error-message">
+            <EmFormErrorMessage formName="email" validatorName="required" />
+            <EmFormErrorMessage formName="email" validatorName="email" />
+          </div>
+        </div>
+        <div>
+          <EmForm formName="password">
+            <input type="password" name="pswd" placeholder="Password" className="form" />
+          </EmForm>
+          <div className="error-message">
+            <EmFormErrorMessage formName="password" validatorName="required" />
+          </div>
+        </div>
+      </EmFormGroup>
+
+      <div>
+        <button className="btn-login" onClick={login}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
